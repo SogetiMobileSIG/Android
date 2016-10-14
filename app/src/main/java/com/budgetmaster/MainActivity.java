439688a625f50cf2bee732af5a213febe2cf9e0c
jpinedo <joep24@gmail.com>
2016-10-13 21:16:28 -0500

    added logout item

--- app/src/main/java/com/budgetmaster/MainActivity.java
+++ app/src/main/java/com/budgetmaster/MainActivity.java
@@ -2,12 +2,34 @@ package com.budgetmaster;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView mainText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);//test commit
         setContentView(R.layout.activity_main);
+        mainText = (TextView)findViewById(R.id.mainText);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId() == R.id.log_out)
+        {
+            mainText.setText(R.string.loggedOut);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_main,menu);
+        return true;
     }
 }
